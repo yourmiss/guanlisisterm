fd096940d4eb44298f6f55061ad9165abeec1442
yourmiss <102975760+yourmiss@users.noreply.github.com>
2023-09-06 16:52:12 +0800

    commit

---
+++ src/setting.ts
@@ -0,0 +1,10 @@
+//用于项目logo|标题配置
+export default {
+  title: '宇哥运营平台', //项目的标题
+  logo: '/jj.jpg', //项目logo设置
+  logos:'/qq.jpg',
+  logo1:'/dd.jpg',
+  logo3:'hh.jpg',
+  logo4:'cc.jpg',
+  logoHidden: true, //logo组件是否隐藏设置
+}
